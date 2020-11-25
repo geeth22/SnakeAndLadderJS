fa47a5a35cbdd966d9d5d6f801d8e0951b2e8b81
geeth22 <geethchandra22@gmail.com>
2020-11-26 00:45:12 +0530

    Check the option to decide type of move

--- utility.js
+++ utility.js
@@ -1,10 +1,36 @@
 let current_position = 0;
-let STARTING_POSITION = 0;
-let ENDING_POSITION = 100;
 
-function RollDice(){
-    var random_check = Math.floor( Math.random() * 6 ) + 1 ;
-    console.log("random check: " + random_check );
+const STARTING_POSITION = 0;
+const ENDING_POSITION = 100;
+const NO_PLAY = 0 ;
+const LADDER = 2 ;
+const SNAKE = 1 ;
+
+function checkOption(){
+    currentPosition = STARTING_POSITION ;
+    while(currentPosition < ENDING_POSITION){
+        var dice = Math.floor( Math.random() * 6 ) + 1 ;
+        console.log("random check: " + dice);
+        var rand = Math.floor( Math.random() * 3 ) ;
+        switch(rand){
+            case LADDER :
+                console.log("LADDER");
+                if(currentPosition + dice <= ENDING_POSITION){
+                currentPosition = currentPosition + dice;
+                }
+                break;
+            case SNAKE :
+                console.log("SNAKE");
+                if(currentPosition - dice >= STARTING_POSITION ){
+                currentPosition = currentPosition - dice;
+                }
+                break;
+            default:
+                console.log("NO PLAY");
+                currentPosition = currentPosition;
+                break;
+        }
+        console.log("current position: " + currentPosition);
+    }
 }
-
-module.exports = { RollDice };
+module.exports = { checkOption };
