0c5e0d892197138490f1ae1b74e63f308feca566
geeth22 <geethchandra22@gmail.com>
2020-11-26 00:40:38 +0530

    Roll the Dice to get value between 1 and 6

--- utility.js
+++ utility.js
@@ -1,3 +1,10 @@
 let current_position = 0;
 let STARTING_POSITION = 0;
 let ENDING_POSITION = 100;
+
+function RollDice(){
+    var random_check = Math.floor( Math.random() * 6 ) + 1 ;
+    console.log("random check: " + random_check );
+}
+
+module.exports = { RollDice };
