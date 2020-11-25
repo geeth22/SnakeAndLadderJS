c5850b2230f22f98e9cfa60f4d95fdcc4ed4afb4
geeth22 <geethchandra22@gmail.com>
2020-11-26 02:49:51 +0530

    refactor:Winner between two players

--- utility.js
+++ utility.js
@@ -1,6 +1,8 @@
 //Variable
 let current_position = 0;
 let Dice_Count = 0;
+let Player1Position ;
+let Player2Position ;
 
 const STARTING_POSITION = 0;
 const ENDING_POSITION = 100;
@@ -8,11 +10,9 @@ const NO_PLAY = 0 ;
 const LADDER = 2 ;
 const SNAKE = 1 ;
 
-function play(){
-    currentPosition = STARTING_POSITION ;
-    while(currentPosition < ENDING_POSITION){
-        var dice = Math.floor( Math.random() * 6 ) + 1 ;
-        console.log("random check: " + dice);
+function play(currentPosition){
+    if(currentPosition < ENDING_POSITION){
+        var dice = Math.floor( Math.random() * 6 ) + 1;
         var rand = Math.floor( Math.random() * 3 ) ;
         switch(rand){
             case LADDER :
@@ -35,6 +35,31 @@ function play(){
         console.log("current position: " + currentPosition);
 	Dice_Count++;
     }
-	console.log("number of times dice rolled: " + Dice_Count);
+	return currentPosition;
 }
-module.exports = { play };
+function TwoPlayers() {
+    Player1Position = STARTING_POSITION;
+     Player2Position = STARTING_POSITION;
+    diceCount = STARTING_POSITION;
+    while( Player1Position < ENDING_POSITION && Player2Position < ENDING_POSITION ){
+        Player1Position = play(Player1Position);
+
+        if(Player1Position == ENDING_POSITION) {
+            break;
+        } 
+
+        Player2Position = play(Player2Position);
+        if(Player2Position == ENDING_POSITION) {
+            break;
+        }
+    }
+}
+function CheckWinner(){
+    if(Player1Position == ENDING_POSITION ){
+        console.log("Player 1 Won The Game ");
+    }
+    else if(Player2Position == ENDING_POSITION ){
+        console.log("Player 2 Won The Game ");
+    }
+}
+module.exports = { play, TwoPlayers, CheckWinner };
